cc305d9519a1a7039ab8b4fe36fab054bf861825
wangshisheng <wssdandan2009@outlook.com>
2020-09-18 08:37:09 +0800

    Update main function

--- inst/motifeRapp/app.R
+++ inst/motifeRapp/app.R
@@ -1326,13 +1326,26 @@ server<-shinyServer(function(input, output, session){
 
   #
   fastaseqout<-reactive({
-    files <- input$fastafile
+    files <<- input$fastafile
     if(is.null(files)){
       datareadfasta<-NULL
     }else{
-      datafasta<-readAAStringSet(files$datapath)
+      datafasta<<-readAAStringSet(files$datapath)
       pro_seqdf<-pro_seqdf1<-as.data.frame(datafasta)
-      pro_seqdf_rown<-unlist(lapply(rownames(pro_seqdf1),function(x) strsplit(x,"\\|")[[1]][2]))
+      pro_seqdf_rown1<-unlist(lapply(rownames(pro_seqdf1),function(x) strsplit(x,"\\|")[[1]][1]))
+      pro_seqdf_rown2<-unlist(lapply(rownames(pro_seqdf1),function(x) strsplit(x,"\\|")[[1]][2]))
+      if(sum(duplicated(pro_seqdf_rown1))>=1 & sum(duplicated(pro_seqdf_rown2))>=1){
+        pro_seqdf_rown<-rownames(pro_seqdf1)
+      }
+      else if(sum(duplicated(pro_seqdf_rown1))>=1){
+        pro_seqdf_rown<-pro_seqdf_rown2
+      }
+      else if(sum(duplicated(pro_seqdf_rown2))>=1){
+        pro_seqdf_rown<-pro_seqdf_rown1
+      }
+      else{
+        pro_seqdf_rown<-rownames(pro_seqdf1)
+      }
       rownames(pro_seqdf1)<-pro_seqdf_rown
       pro_seqdfncar<-unlist(lapply(pro_seqdf1$x,nchar))
       pro_seqdf<-pro_seqdf1[pro_seqdfncar>20,,drop=FALSE]
